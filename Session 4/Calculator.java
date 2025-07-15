4ba86d6ec7695b7a19a5fbad671f32aedb135673
Adele220415 <oluwatumininusolanke@gmail.com>
2025-07-15 14:16:20 +0100

    Add files via upload

---
+++ Session 4/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator{
+    int square(int a){
+        return a*a;
+    }
+
+    double square(double a){
+        return a*a;
+    }
+
+}
+// public class Main{
+//     public static void main(String[] args){
+//         Calculator.calc = new Calculator();
+//         System.out.println(calc.square(4));
+//         System.out.println(calc.square(2));
+//     }
+// }
---
+++ Session 4/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+    public String brand;
+    private int regNumber;
+
+    public int getRegNumber(){
+        return regNumber;
+    }
+}
